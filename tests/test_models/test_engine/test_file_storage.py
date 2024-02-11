4e9a35691f760e104a138ecda3f5eb90d1aa5463
mofirojean <mofirojean@gmail.com>
2024-02-11 13:28:38 +0100

    (AB) created test for file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+FileStorage test module
+"""
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instantiation(self):
+        """Test instance of the file storage class"""
+        storage = FileStorage()
+        self.assertIsInstance(storage, FileStorage)
+
+
+if __name__ == "__main__":
+    unittest.main()
