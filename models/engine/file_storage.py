9d510e08a1a8f398600aa5387ae359f4d7225c44
mofirojean <mofirojean@gmail.com>
2024-02-11 13:11:46 +0100

    (AB) created the file storage program

---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+File storage module engine
+"""
+import os
+import json
+
+
+class FileStorage:
+    """Serialization of instance to JSON and Deserialize JSON to instance"""
+    __objects: dict = dict()
+    __file_path: str = 'file.json'
+
+    def all(self):
+        """Returns the dictionary objects"""
+        return type(self).__objects
+
+    def save(self):
+        """Serialization of objects to JSON file (path: __file_path)"""
+        new_obj = type(self).__objects
+        path = type(self).__file_path
+        with open(path, 'w', encoding='UTF-8') as file:
+            json.dump(
+                {key: value.to_dict() for key, value in new_obj.items()},
+                file,
+                indent=4
+            )
+
+    def new(self, obj):
+        """Puts in __objects the obj with key <obj class name>.id"""
+        type(self).__objects[f"{obj.__class__.__name__}"] = obj
+
+    def reload(self):
+        """
+        Deserialisation of the JSON file to __objects
+        (only if the JSON file exists; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
+        path = type(self).__file_path
+        if os.path.exists(path):
+            with open(path, "r") as file:
+                FileStorage.__objects = json.load(file)
